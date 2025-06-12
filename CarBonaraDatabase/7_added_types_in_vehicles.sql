7e47c102c6bf651fddb7e7fa495e6a0c9a509c55
Anrew <andrvolg.job@gmail.com>
2025-06-12 14:10:25 -0400

    added sql doc with adding types in vehicles

---
+++ CarBonaraDatabase/7_added_types_in_vehicles.sql
@@ -0,0 +1,7 @@
+SELECT * FROM car_dealership.vehicles;
+
+UPDATE vehicles SET type = 'Truck' WHERE VIN = '1FTFW1EF3FEA12345';     -- Ford F-150
+UPDATE vehicles SET type = 'Sedan' WHERE VIN = '1HGCM82633A004352';     -- Honda Civic
+UPDATE vehicles SET type = 'Sedan' WHERE VIN = '1N4AL11D75C109151';     -- Nissan Altima
+UPDATE vehicles SET type = 'Sedan' WHERE VIN = '3FAHP0HA7AR123456';     -- Ford Fusion
+UPDATE vehicles SET type = 'Sedan' WHERE VIN = 'JH4KA8260MC001234';     -- Acura Legend
